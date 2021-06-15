3350585b0adc911878de0bec2a63a7c84c370d02
Lee Taylor <leetaylor5455@yahoo.co.uk>
2021-06-16 00:28:37 +0100

    Disable CORS for hosting on https server

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 
 app.use('/', express.static('public'));
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Expose-Headers', 'Content-Length');
+    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+    if (req.method === 'OPTIONS') {
+      return res.send(200);
+    } else {
+      return next();
+    }
+});
 
 // PORT
 const port = process.env.PORT || 5000
